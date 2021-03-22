b09c125260c50121a705f3dd528b8e235462a0e6
Elena Porras <m.elenaporras@hotmail.com>
2021-03-22 11:23:37 +0100

    Movie json

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,1 +1,29 @@
 // JavaScript source code
+let batman = {
+    "title": "The Dark Knight",
+    "url": "https://www.youtube.com/watch?v=EXeTwQWrcwY"
+}
+
+let matrix = {
+    "title": "The Matrix",
+    "url": "https://www.youtube.com/watch?v=vKQi3bBA1y8"
+}
+let ib = {
+    "title": "Inglorious Bastards",
+    "url": "https://www.youtube.com/watch?v=KnrRy6kSFF0"
+}
+
+let jp = {
+    "title": "Jurassic Park",
+    "url": "https://www.youtube.com/watch?v=lc0UehYemQA"
+}
+
+let samurai = {
+    "title": "The Last Samurai",
+    "url": "https://www.youtube.com/watch?v=T50_qHEOahQ"
+}
+
+let ff = {
+    "title": "Fast and Furious 9",
+    "url": "https://www.youtube.com/watch?v=FUK2kdPsBws"
+}
